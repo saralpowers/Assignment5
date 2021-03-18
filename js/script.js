dee58caafbb6f0b11a8197afd2bce4092a010305
saralpowers <63485240+saralpowers@users.noreply.github.com>
2021-03-18 13:56:22 -0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
 window.addEventListener('load', () => {
 
-    var form = window.document.getElementById('empForm');
+    let form = window.document.getElementById('empForm');
 
-    function process(e) {
-        id = document.getElementById("getInput");
-        nameo = document.getElementById('userInput');
-        ext = document.getElementById('userInput');
-        email = document.getElementById('userInput');
-        department = document.getElementById('userInput');
+    function saveval(e) {
+        window.console.log(`${e.target.id}: ${e.target.value}`);
     }
 
-    form.addEventListener('change', process, true);
+    empForm.addEventListener('change', saveval, {
+    }, false);
 
     form.addEventListener('submit', event => {
         event.preventDefault();
-        console.log(`ID: ${id}`);
-        console.log(`Name: `);
-        console.log(`Extension: `);
-        console.log(`Email: `);
-        console.log(`Department: `);
+        window.console.log()
     })
 });
 
