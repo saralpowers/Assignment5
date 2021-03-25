597de6957c4dab02dfbd610725f052e5d4888dec
saralpowers <saralpowers@gmail.com>
2021-03-25 11:18:32 -0700

    resubmit assignment

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-window.addEventListener('load', () => {
-
-    let form = window.document.getElementById('empForm');
-
-    function saveval(e) {
-        window.console.log(`${e.target.id}: ${e.target.value}`);
-    }
-
-    empForm.addEventListener('change', saveval, {
-    }, false);
-
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        window.console.log()
-    })
-});
-
-
-
-
+window.addEventListener('load', (e) => {
+    let form = document.querySelector('#empForm');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let empId = window.document.getElementById('id').value;
+        console.log(`ID: ${empId}`);
+        let myName = window.document.getElementById("name").value;
+        console.log(`Name: ${myName}`);
+        let extension = window.document.getElementById("ext").value;
+        console.log(`Extension: ${extension}`);
+        let email = window.document.getElementById("email").value;
+        console.log(`Email: ${email}`);
+        let department = window.document.getElementById("department").value;
+        console.log(`Department: ${department}`);
+    });});
